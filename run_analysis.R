e7e5230c87b3d98af089ec585ac209fa26caabe1
nihrao <nihrao@gmail.com>
2015-11-21 20:02:05 -0500

    test

--- run_analysis.R
+++ run_analysis.R
@@ -1,1 +1,47 @@
+dir <- "/Users/Nihal/CourseraR/GCdata/"
 
+training <- read.csv(paste(dir,"X_train.txt",sep=''), sep="", header=FALSE)
+training[,562] <- read.csv(paste(dir,"y_train.txt",sep=''), sep="", header=FALSE)
+training[,563] <- read.csv(paste(dir,"subject_train.txt",sep=''), sep="", header=FALSE)
+
+testing <- read.csv(paste(dir,"X_test.txt",sep=''), sep="", header=FALSE)
+testing[,562] <- read.csv(paste(dir,"y_test.txt",sep=''), sep="", header=FALSE)
+testing[,563] <- read.csv(paste(dir,"subject_test.txt",sep=''), sep="", header=FALSE)
+
+activityLabels <- read.csv(paste(dir,"activity_labels.txt",sep=''), sep="", header=FALSE)
+
+# Read features and make the feature names better suited for R with some substitutions
+features = read.csv(paste(dir,"features.txt",sep=''), sep="", header=FALSE)
+features[,2] = gsub('-mean', 'Mean', features[,2])
+features[,2] = gsub('-std', 'Std', features[,2])
+features[,2] = gsub('[-()]', '', features[,2])
+
+# Merge training and test sets together
+allData = rbind(training, testing)
+
+# Get only the data on mean and std. dev.
+colsWeWant <- grep(".*Mean.*|.*Std.*", features[,2])
+# First reduce the features table to what we want
+features <- features[colsWeWant,]
+# Now add the last two columns (subject and activity)
+colsWeWant <- c(colsWeWant, 562, 563)
+# And remove the unwanted columns from allData
+allData <- allData[,colsWeWant]
+# Add the column names (features) to allData
+colnames(allData) <- c(features$V2, "Activity", "Subject")
+colnames(allData) <- tolower(colnames(allData))
+
+currentActivity = 1
+for (currentActivityLabel in activityLabels$V2) {
+  allData$activity <- gsub(currentActivity, currentActivityLabel, allData$activity)
+  currentActivity <- currentActivity + 1
+}
+
+allData$activity <- as.factor(allData$activity)
+allData$subject <- as.factor(allData$subject)
+
+tidy = aggregate(allData, by=list(activity = allData$activity, subject=allData$subject), mean)
+# Remove the subject and activity column, since a mean of those has no use
+tidy[,90] = NULL
+tidy[,89] = NULL
+write.table(tidy, paste(dir,"tidy.txt",sep=''), sep="\t")
